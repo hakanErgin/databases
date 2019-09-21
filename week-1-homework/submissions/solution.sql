6939e90b3f07905955f30f097a56af1dbab6c160
hakanErgin <hakan.ergin@gmail.com>
2019-09-21 13:31:04 +0200

    UPDATE

--- week-1-homework/submissions/solution.sql
+++ week-1-homework/submissions/solution.sql
@@ -1,18 +1,13 @@
 --- 1.Which car has the highest accelerate value?
 
-SELECT max(accelerate)
-FROM cars_data;
-
-
-SELECT *
-FROM cars_data
-WHERE accelerate="25";
-
-
-SELECT *
+SELECT make
 FROM car_names
-WHERE id="307"
-  OR id="403";
+WHERE id IN
+    (SELECT id
+     FROM cars_data
+     WHERE accelerate IN
+         (SELECT max(accelerate)
+          FROM cars_data));
 
 --- 2.List the weight of the cars made in the 1980s (1980 and later).
 
@@ -28,30 +23,24 @@ WHERE model="chevrolet";
 
 --- 4.What is the full name of the maker of the plymouth model?
 
-SELECT *
-FROM models
-WHERE name="plymouth";
-
-
 SELECT full_name
 FROM car_makers
-WHERE id="6";
+WHERE id IN
+    (SELECT maker
+     FROM models
+     WHERE name="plymouth");
 
 --- 5.Which continent is the Volvo car maker from?
 
-SELECT *
-FROM car_makers
-WHERE maker= "volvo";
-
-
-SELECT *
-FROM countries
-WHERE id="6";
-
-
-SELECT *
+SELECT continent
 FROM continents
-WHERE id="2";
+WHERE id IN
+    (SELECT continent
+     FROM countries
+     WHERE id IN
+         (SELECT country
+          FROM car_makers
+          WHERE maker= "volvo"));
 
 --- 6.How many car models are audi?
 
@@ -67,37 +56,35 @@ WHERE maker LIKE "s%";
 
 --- 8.How many cars have a horsepower more than 100 but less than 200?
 
-SELECT count(*)
+SELECT count(id)
 FROM cars_data
 WHERE horsepower BETWEEN 100 AND 200;
 
 --- 9.List all car makers from australia.
 
-SELECT *
+SELECT full_name
 FROM car_makers
-WHERE country="11";
+WHERE country IN
+    (SELECT id
+     FROM countries
+     WHERE name ="australia");
 
 --- 10.List all car makers not from sweden, japan, france or germany.
 
-SELECT *
+SELECT maker
 FROM car_makers
-WHERE country!="6"
-  AND country!="3"
-  AND country!="2";
+WHERE country IN
+    (SELECT id
+     FROM countries
+     WHERE name NOT IN ('sweden',
+                        'japan',
+                        'france',
+                        'germany'));
 
 --- bonus 1.List all cars where the make starts with an a and ends with an r.
 
 SELECT *
 FROM car_names
-WHERE make LIKE "a%"
-  AND make LIKE "%r";
+WHERE make LIKE "a%r";
 
 --- bonus 2.How many cars have more than 6 cylinders, weight less than 4000 but more than 3000, and was not made in the years 1970, 1975 or 1981.
-
-SELECT *
-FROM cars_data
-WHERE cylinders>"6"
-  AND weight BETWEEN 3000 AND 4000
-  AND YEAR !="1970"
-  AND YEAR !="1975"
-  AND YEAR !="1981";
